4380e5ea6e7b356295bc2cd51fa29bb66bb006e6
Mayra <mayra.cademartori@gmail.com>
2023-07-07 23:18:11 -0300

    feat: template router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from '~/server/api/trpc';
 import { committeeRouter } from './routers/committee';
 import { employeeRouter } from './routers/employee';
 import { membershipRouter } from './routers/membership';
+import { templateRouter } from './routers/template';
 
 /**
  * This is the primary router for your server.
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   committee: committeeRouter,
   employee: employeeRouter,
   membership: membershipRouter,
+  template: templateRouter,
 });
 
 // export type definition of API
