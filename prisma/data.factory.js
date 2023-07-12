68dfdd07c154c859204282080844051c0a57a1be
Mayra <mayra.cademartori@gmail.com>
2023-07-12 16:02:55 -0300

    fix seed error

--- prisma/data.factory.js
+++ prisma/data.factory.js
@@ -39,7 +39,7 @@ class DataFactory {
     return {
       employee_id: mockEmployeeId,
       committee_id: mockCommitteeId,
-      //begin_date: faker.date.past(),
+      begin_date: null,
       term: +faker.number.int({ min: 1, max: 5 }),
       observations: faker.lorem.sentence(),
       is_active: faker.datatype.boolean({ probability: 0.75 }),
