eee47e6bd51037574386fbb124e27c52b27e4bc2
Mayra <mayra.cademartori@gmail.com>
2023-08-12 17:28:55 -0300

    log

--- src/pages/api/notifications/index.ts
+++ src/pages/api/notifications/index.ts
@@ -2,17 +2,9 @@ import { Template, Committee } from '@prisma/client'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getEmails, getNotifications, updateLastSent } from '~/server/api/routers/template'
 import { sendEminentElectionNotification } from '~/server/auth/email'
-import { api } from '~/utils/api'
 
 export default async function handler(request: NextApiRequest, res: NextApiResponse) {
-  //const { searchParams } = request.nextUrl;
-  //const hasTitle = searchParams.has('title');
-  // const title = hasTitle
-  //   ? searchParams.get('title')?.slice(0, 100)
-  //   : 'My default title';
-
-  //return res.status(500).send(title);
-
+  console.log('Running notifications job...')
   try {
     const templates = await getNotifications()
     const emails = await getEmails()
@@ -21,7 +13,7 @@ export default async function handler(request: NextApiRequest, res: NextApiRespo
         sendEminentElectionNotification(e.email!, templates as any)
       )
       await Promise.all(promises)
-      await updateLastSent(templates)
+      updateLastSent(templates)
       return res.status(200).json({
         body: 'Success!',
         cookies: request.cookies
