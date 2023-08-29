4ca111026877b0823c22bd2b2e7ddde983cd32db
Mayra <mayra.cademartori@gmail.com>
2023-08-29 17:14:10 -0300

    color design changes

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,23 +1,22 @@
-import { sysVersion } from '@/package.json'
-
 export const Footer = () => {
   return (
     <div className="mt-7 flex w-full items-center justify-center">
       <div className=" absolute bottom-0 flex h-[20px] w-full flex-row items-center bg-[--bottom-background] py-4 shadow-lg">
-        <div className="container flex flex-row text-xs font-semibold tracking-wider text-white">
+        <div className="container flex flex-row text-xs tracking-wider text-white">
           <span>
             <span className="mr-[10px]">♥</span>
             Feedback? Contate{' '}
             <a
               target="_blank"
               href="mailto:mayra.cademartori@gmail.com"
-              className="hover:underline"
+              className="font-semibold hover:underline"
             >
               Mayra Cademartori
             </a>
           </span>
-          <span className="ml-auto">
-            Committee Management System <span className="mx-1 py-[2px]">|</span> Versão {sysVersion}
+          <span className="ml-auto ">
+            <span className="font-semibold">Committee Management System</span>{' '}
+            <span className="mx-1 py-[2px]">|</span> Versão 2.1.1
           </span>
         </div>
       </div>
